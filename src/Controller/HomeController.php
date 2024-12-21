3299008a2c6f44b1fb7d2fec8aea08eefd99c88f
Trev0r48 <hamada.hea55@gmail.com>
2024-12-21 10:36:02 +0100

    form creation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use App\Form\PersonneType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,4 +21,28 @@ class HomeController extends AbstractController
     public function about(): Response {
         return new Response(content:"hello world 2");
     }
+
+    #[Route('/dummy', name: 'app_dummy')]
+    public function dummy(): Response
+    {
+        $text = "hello this is home";
+        $tabyear = [2020,2021,2022,2023];
+
+        return $this->render('home/dummy.html.twig',[
+            "text" => $text,
+            "years" => $tabyear
+        ]);
+        ;
+    }
+
+    #[Route('/displayform', name: 'app_displayform')]
+    public function displayForm(Request $request): Response {
+        $hamada= new Personne();
+        $personneForm = $this->createForm(PersonneType::class,$hamada);
+        return $this->render('home/displayForm.html.twig',[
+            "form" => $personneForm
+        ]);
+        ;
+    }
+
 }
---
+++ src/Entity/Personne.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
+class Personne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $age = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 4)]
+    private ?string $salary = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getSalary(): ?string
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(string $salary): static
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+}
